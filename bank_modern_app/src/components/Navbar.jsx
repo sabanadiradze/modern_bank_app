1d08d0fd103eddd4e1204afa831d230a6e8b49ba
[SabaNadiradze] <nadiradzes96@gmail.com>
2023-09-22 23:31:07 +0300

    Update Navbar.jsx

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -11,17 +11,17 @@ const Navbar = () => {
       <ul className='list-none sm:flex' hidden justify-end items-center flex-1>
         {navLinks.map((nav, index) => (
           <li
-            key={nav.id}className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'} text-white`}>
+            key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-5' : 'mr-10'} text-white`}>
 
-            
 
-        <a href={'#${nav.id'}>
-          {nav.title}
-        </a>
 
-      </li>
-      ))}
-    </ul>
+            <a href={'#${nav.id'}>
+              {nav.title}
+            </a>
+
+          </li>
+        ))}
+      </ul>
     </nav >
   )
 }
