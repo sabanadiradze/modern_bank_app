9b5453538da960958228fab83ac5177750ddb493
[SabaNadiradze] <nadiradzes96@gmail.com>
2023-09-24 19:41:45 +0300

    navbar

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <ul className='list-none sm:flex' hidden justify-end items-center flex-1>
         {navLinks.map((nav, index) => (
           <li
-            key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-5' : 'mr-10'} text-white`}>
+            key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
 
 
 
