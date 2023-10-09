10517504494e8590fe38377b49a2739e245b86a1
[SabaNadiradze] <nadiradzes96@gmail.com>
2023-10-10 01:35:28 +0300

    fixed the width of the testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ import FeedbackCard from './FeedbackCard';
 const Testimonials = () => {
   return (
     <section id="clients" className={`styles.paddingY ${styles.flexCenter} flex-col relative `  }>
-      <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'> </div>
+      <div className='absolute z-[0] w-[60%] h-[60%]  rounded-full blue__gradient'> </div>
         <div/>
         <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'> 
           <h1 className={`${styles.heading2} ${styles.flexCenter}`}>What people are <br className='sm:block hidden'/>saying about us</h1>
