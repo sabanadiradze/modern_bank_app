b6798e3d1984eaf3fad768f7dce373bc6cbe20e9
[SabaNadiradze] <nadiradzes96@gmail.com>
2023-10-09 23:13:42 +0300

    finished landing page parts
    finished the rest of the parts for the landing page

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppiuns font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppiuns font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
       Get Started
     </button>
   )
